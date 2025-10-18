d8f1609da022df16c14348b402fac3416ec9b764
David Carrillo <carrillo.dact13@gmail.com>
2025-10-18 14:50:54 -0700

    form hooks

--- React-Course/react-app/src/components/Form.tsx
+++ React-Course/react-app/src/components/Form.tsx
@@ -1,19 +1,85 @@
-import React from "react";
+import React, { FormEvent, useRef, useState } from "react";
+
+// Getting input fields using 'useRef'
+// const Form = () => {
+//   const nameRef = useRef<HTMLInputElement>(null);
+//   const ageRef = useRef<HTMLInputElement>(null);
+//   const person = { name: "", age: 0 };
+
+//   const handleSubmit = (event: FormEvent) => {
+//     event.preventDefault();
+//     if (nameRef.current !== null) person.name = nameRef.current.value;
+//     if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
+//     console.log(person);
+//   };
+//==============================================================================
+
+// Getting input fields using 'useState'
+// const Form = () => {
+//   const [person, setPerson] = useState({
+//     name: "",
+//     age: "",
+//   });
+
+//   const handleSubmit = (event: FormEvent) => {
+//     event.preventDefault();
+//     console.log(person);
+//   };
+//==============================================================================
+
+// Getting input fields using react-hook-form 'useForm'
+import { FieldValues, useForm } from "react-hook-form";
+
+interface FormData {
+  name: string;
+  age: number;
+}
 
 const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>();
+  console.log(errors);
+
+  const onSubmit = (data: FieldValues) => console.log(data);
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="mb-3">
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" type="text" className="form-control" />
+        <input
+          {...register("name", { required: true, minLength: 3 })}
+          id="name"
+          type="text"
+          className="form-control"
+        />
+        {errors.name?.type === "required" && (
+          <p className="text-danger">The name field is required.</p>
+        )}
+        {errors.name?.type === "minLength" && (
+          <p className="text-danger">
+            The name must be at least 3 characters long.
+          </p>
+        )}
       </div>
       <div className="mb-3">
-        <label htmlFor="age" className="form-label">Age</label>
-        <input id="age" type="number" className="form-control" />
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input
+          {...register("age")}
+          id="age"
+          type="number"
+          className="form-control"
+        />
       </div>
-      <button className="btn btn-primary" type="submit">Submit</button>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
     </form>
   );
 };
