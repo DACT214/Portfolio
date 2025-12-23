ce8158e743543f7a662c702fa2931c5f9e171c07
David Carrillo <carrillo.dact13@gamil.com>
2025-12-23 14:34:53 -0800

    Implement Searching

--- React-Course/game-hub/src/main.tsx
+++ React-Course/game-hub/src/main.tsx
@@ -2,6 +2,7 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.css";
 import theme from "./theme";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
