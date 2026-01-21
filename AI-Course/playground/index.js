1d35440c642c2ad0155600c7cebe2efba07ec049
David Carrillo <carrillo.dact13@gamil.com>
2026-01-21 13:37:22 -0800

    adding .env

--- AI-Course/playground/index.js
+++ AI-Course/playground/index.js
@@ -1,13 +1,3 @@
-// // CommonJS syntax
-// const { get_encoding } = require('tiktoken');
-
-// ES module syntax
-import { get_encoding } from "tiktoken";
-
-// token ID -> token
-// 904 -> hello
-const encoding = get_encoding("cl100k_base");
-const tokens = encoding.encode(
-  "Hello world! This is the first test of tiktoken library.",
-);
-console.log(tokens);
+import dotenv from "dotenv";
+dotenv.config();
+const OPENAI_API_KEY = process.env.API_KEY;
