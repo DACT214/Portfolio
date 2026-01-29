2cd8399cdb9678d5da309fa3df5106026cbcc4d8
David Carrillo <carrillo.dact13@gamil.com>
2026-01-29 11:58:27 -0800

    Connect the client and server

--- AI-Course/my-app/packages/server/index.ts
+++ AI-Course/my-app/packages/server/index.ts
@@ -4,7 +4,10 @@ const app = express();
 const port = Bun.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send(Bun.env.OPENAI_API_KEY + "!");
+  res.send("Hola Mundo");
+});
+app.get("/api/hello", (req, res) => {
+  res.json({ message: "hello world" });
 });
 
 app.listen(port, () => {
